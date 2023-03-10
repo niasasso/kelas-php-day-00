495c2442d3f365d72bd0202b10ebb8c617b9d190
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2023-03-10 10:48:27 +0900

    coding php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	// Variabel String
+	$judul = "Hello World!!";
+	$bilangan = 10;
+	$nama = "Hello Acho!";
+?>
+
+
+
+<html>
+	<head>
+		<title>PHP Day 00</title>
+	</head>
+
+<body style="padding: 10px; text-align: center;">
+
+	<h1>PHP Day 00 - File PHP</h1>
+
+	<h2> <?php echo $judul; ?> </h2>
+	<h2> <?= $bilangan; ?> </h2>
+	<h2> <?= $nama; ?> </h2>
+
+</body>
+
+</html>
